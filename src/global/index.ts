22f4dbca6203607724f0216a4924a8527f0e6b2d
lixinran <821336481@qq.com>
2022-04-25 17:32:25 +0800

    fix: any 类型不做强制性eslint

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,5 +1,5 @@
 import { App } from 'vue'
 import regesterProperties from './registerProperties'
 export function globalRegister(app: App) {
-  // app.use(regesterProperties)
+  app.use(regesterProperties)
 }
